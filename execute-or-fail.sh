775630752a8920c938a6b0bf94426d93fb15c563
konard <drakonard@gmail.com>
2021-11-09 12:31:18 +0300

    Closes #2

--- execute-or-fail.sh
+++ execute-or-fail.sh
@@ -2,16 +2,14 @@
 # set -e
 
 TOKEN_EXPIRED_ERROR_MESSAGE="User authorization failed: access_token has expired."
+TOKEN_WAS_GIVEN_TO_ANOTHER_IP_ERROR_MESSAGE="User authorization failed: access_token was given to another ip address."
 QUERY_RESULT=$(curl -s "$1")
 
 ERROR_MESSAGE=$(echo "$QUERY_RESULT" | jq .error.error_msg | tr -d '"')
 
 echo "$QUERY_RESULT" | jq
 
-if [ "$ERROR_MESSAGE" = "$TOKEN_EXPIRED_ERROR_MESSAGE" ]; then
-  # echo "$ERROR_MESSAGE"
-  # exit 1
-
+if [ "$ERROR_MESSAGE" = "$TOKEN_EXPIRED_ERROR_MESSAGE" ] || [ "$ERROR_MESSAGE" = "$TOKEN_WAS_GIVEN_TO_ANOTHER_IP_ERROR_MESSAGE" ]; then
   # Get new access token
   EMAIL=`cat email`
   PASS=`cat pass`
