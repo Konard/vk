02dbd3e2d25882acfd5b452bc538853f474c86a2
konard <drakonard@gmail.com>
2021-11-09 15:15:09 +0300

    Script name fix.

--- accept-all-friend-requests-once.sh
+++ accept-all-friend-requests-once.sh
@@ -3,6 +3,6 @@ set -e
 
 ACCESS_TOKEN=`cat access-token`
 
-./execute-or-fail.sh "https://api.vk.com/method/execute.acceptAllFriendRequests?access_token=${ACCESS_TOKEN}&v=5.131"
+./execute.sh "https://api.vk.com/method/execute.acceptAllFriendRequests?access_token=${ACCESS_TOKEN}&v=5.131"
 
 echo
--- delete-inactive-friends.sh
+++ delete-inactive-friends.sh
@@ -3,6 +3,6 @@ set -e
 
 ACCESS_TOKEN=`cat access-token`
 
-./execute-or-fail.sh "https://api.vk.com/method/execute.deleteInactiveFriends?access_token=${ACCESS_TOKEN}&v=5.131&offset=$1"
+./execute.sh "https://api.vk.com/method/execute.deleteInactiveFriends?access_token=${ACCESS_TOKEN}&v=5.131&offset=$1"
 
 echo
