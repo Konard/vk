ef833666d33b63a7e32391da103080efb3b7dc60
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2021-11-30 15:26:08 +0300

    Update deleteFirstDeactivatedFriend.js

--- Application/deleteFirstDeactivatedFriend.js
+++ Application/deleteFirstDeactivatedFriend.js
@@ -1,33 +1,24 @@
 var friendsOffset = 0;
 var step = 5000;
-var operations = 2;
+var deactivatedValues = ["banned", "deleted"];
+var i, deactivatedIndex, friendId;
 var deletedFriendsIds = [];
-while(friendsOffset < 10000)
+while (friendsOffset < 10000)
 {
-  var friends = API.friends.get({ offset: friendsOffset });
-  var dFriends = friends.items@.deactivated;
-  var deactivatedIndex = dFriends.indexOf("banned");
-  deactivatedIndex = deactivatedIndex + dFriends.indexOf("deleted");
-  while ((deactivatedIndex > 0) && (deletedFriendsIds.length < operations)){
-    var userIdToDelete = friends.items[deactivatedIndex].id;
-    if (friends.items@.deactivated){
-      deletedFriendsIds.push(userIdToDelete);
-      API.friends.delete({ user_id: userIdToDelete });
-      if(deletedFriendsIds.length >= operations) {
-        return "deleted friends: " + deletedFriendsIds;
-      }
-      dFriends = friends.items.slice(deactivatedIndex);
-      dFriends = dFriends.slice(deactivatedIndex);
-      friends.items = friends.items.slice(deactivatedIndex);
+  var friends = API.friends.get({ offset: friendsOffset, count: step, fields: "deactivated", order: "deactivated" }).items;
+  var deactivatedFields = friends@.deactivated;
+  i = 0;
+  while (i < deactivatedValues.length) 
+  {
+    deactivatedIndex = deactivatedFields.indexOf(deactivatedValues[i]);
+    if (deactivatedIndex > 0)
+    {
+      friendId = friends[deactivatedIndex].id;
+      deletedFriendsIds.push(friendId);
+      API.friends.delete({ user_id: friendId });
     }
+    i = i + 1;
   }
   friendsOffset = friendsOffset + step;
 }
-if(deletedFriendsIds.length <= 0) 
-{
-  return "no deactivated friends";
-}
-else
-{
-  return "deleted friends: " + deletedFriendsIds;
-}
+return "deleted friends: [" + deletedFriendsIds + "]";
