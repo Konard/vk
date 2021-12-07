abcb2ca92cbbd5c85515281d3bbcbb379a40b572
konard <drakonard@gmail.com>
2021-12-07 13:10:05 +0300

    A better scripts execution timing.

--- do-all.sh
+++ do-all.sh
@@ -3,13 +3,19 @@ set -e
 
 ./handle-requests.sh
 
+./delete-first-deactivated-friend.sh
+
+sleep 16
+
+./accept-all-friend-requests-once.sh
+
 while :
 do
+  sleep 600
+
   ./delete-first-deactivated-friend.sh
 
   sleep 600
 
   ./accept-all-friend-requests-once.sh
-
-  sleep 600
 done
