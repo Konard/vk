97e07cedc7367503f68336198c806213b874df32
konard <drakonard@gmail.com>
2021-12-07 13:10:59 +0300

    Another error message is handled now.

--- execute.sh
+++ execute.sh
@@ -2,13 +2,15 @@
 
 TOKEN_EXPIRED_ERROR_MESSAGE="User authorization failed: access_token has expired."
 TOKEN_WAS_GIVEN_TO_ANOTHER_IP_ERROR_MESSAGE="User authorization failed: access_token was given to another ip address."
+NO_TOKEN_PASSED_ERROR_MESSAGE="User authorization failed: no access_token passed."
+
 QUERY_RESULT=$(curl -s "$1")
 
 ERROR_MESSAGE=$(echo "$QUERY_RESULT" | jq .error.error_msg | tr -d '"')
 
 echo "$QUERY_RESULT" | jq
 
-if [ "$ERROR_MESSAGE" = "$TOKEN_EXPIRED_ERROR_MESSAGE" ] || [ "$ERROR_MESSAGE" = "$TOKEN_WAS_GIVEN_TO_ANOTHER_IP_ERROR_MESSAGE" ]; then
+if [ "$ERROR_MESSAGE" = "$TOKEN_EXPIRED_ERROR_MESSAGE" ] || [ "$ERROR_MESSAGE" = "$TOKEN_WAS_GIVEN_TO_ANOTHER_IP_ERROR_MESSAGE" ] || [ "$ERROR_MESSAGE" = "$NO_TOKEN_PASSED_ERROR_MESSAGE" ]; then
   # Get new access token
   EMAIL=`cat email`
   PASS=`cat pass`
