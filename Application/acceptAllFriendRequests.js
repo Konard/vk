d1b7bc2a9b440c4a9a13b2b27e9a9631b3abd63c
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2021-12-10 13:03:28 +0300

    new acceptAllFriendRequests.js

--- Application/acceptAllFriendRequests.js
+++ Application/acceptAllFriendRequests.js
@@ -2,9 +2,10 @@ var requests = API.friends.getRequests().items;
 var i = 0;
 while(i < requests.length)
 {
-  if(i < 24){
+  if(i < 24)
+  {
     API.friends.add({ user_id: requests[i] });
-    i = i + 1;
   }
+  i = i + 1;
 }
 return requests;
