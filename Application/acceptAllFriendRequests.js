22e145bde6bea1fd438524d249be212e75ca75a5
Не Еремей! <60701195+eremeyko@users.noreply.github.com>
2021-12-10 15:18:27 +0300

    Update Application/acceptAllFriendRequests.js
    Co-authored-by: Konstantin Dyachenko <drakonard@gmail.com>

--- Application/acceptAllFriendRequests.js
+++ Application/acceptAllFriendRequests.js
@@ -2,10 +2,7 @@ var requests = API.friends.getRequests().items;
 var i = 0;
 while(i < requests.length)
 {
-  if(i < 24)
-  {
-    API.friends.add({ user_id: requests[i] });
-  }
+  API.friends.add({ user_id: requests[i] });
   i = i + 1;
 }
 return requests;
