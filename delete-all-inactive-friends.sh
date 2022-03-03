cba858e4512a6f2bfcdbcb001231aa9c147a42e5
Konstantin Dyachenko <drakonard@gmail.com>
2022-03-03 13:35:07 +0300

    Making deletion of inactive friends stopable.

--- delete-all-inactive-friends.sh
+++ delete-all-inactive-friends.sh
@@ -6,6 +6,18 @@ while [ $i -ne 36 ]
 do
   ./delete-inactive-friends.sh "$i"
   i=$(($i+1))
+ 
+  read -p "Continue? [y / n] " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
 
-  sleep 1
+  # nothing
+
+  else
+  
+  echo "Inactive friends deletion is stopped."
+  exit 0
+
+  fi
 done
