e7dc0958289c21ec47fc894a2156aba33031b35e
Konstantin Dyachenko <drakonard@gmail.com>
2022-03-05 13:10:01 +0300

    Update deleteInactiveFriends.js

--- Application/deleteInactiveFriends.js
+++ Application/deleteInactiveFriends.js
@@ -1,15 +1,16 @@
 var friendsLimit = 10000;
 var step = 23;
-var timeDifference = 432000; // 5 days
+var timeDifference = 259200; // 3 days
+var friend_id_barier = 10000000; // do not delete users with id less than this number
 var friendsOffset = friendsLimit - step * (parseInt(Args.offset) + 1);
 var friends = API.friends.get({ count: step, offset: friendsOffset, fields: "last_seen" }).items;
-var timebarier = API.utils.getServerTime() - timeDifference;
+var time_barier = API.utils.getServerTime() - timeDifference;
 var i = 0;
 var deletedFriendsIds = [];
 while(i < friends.length)
 {
   var last_seen_time = friends[i].last_seen.time;
-  if (last_seen_time != null && (last_seen_time > 0) && (last_seen_time <= timebarier) && (friends[i].id > 30000000)) {
+  if (last_seen_time != null && (last_seen_time > 0) && (last_seen_time <= time_barier) && (friends[i].id > friend_id_barier)) {
     API.friends.delete({ user_id: friends[i].id });
     deletedFriendsIds.push(friends[i].id);
   }
