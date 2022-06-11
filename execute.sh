89bd6d823c99ad9187eb7a29da608d3bbaa52fcd
Konstantin Dyachenko <drakonard@gmail.com>
2022-06-11 12:29:09 +0300

    Better token extraction.

--- execute.sh
+++ execute.sh
@@ -15,7 +15,7 @@ if [ "$ERROR_MESSAGE" = "$TOKEN_EXPIRED_ERROR_MESSAGE" ] || [ "$ERROR_MESSAGE" =
   EMAIL=`cat email`
   PASS=`cat pass`
   cat vk_auth_token.side | jq '.tests[0].commands[2].value="'"$EMAIL"'" | .tests[0].commands[4].value="'"$PASS"'"' > vk_auth_token.temp
-  selenium-side-runner vk_auth_token.temp | grep -o "https.*" | sed -r "s|.*access_token=([0-9a-f]+).*|\1|" > access-token
+  selenium-side-runner vk_auth_token.temp | grep -o "https.*" | sed -r "s|.*access_token=([^&]+)&.*|\1|" > access-token
   rm vk_auth_token.temp
   echo "Token is updated"
 fi;
