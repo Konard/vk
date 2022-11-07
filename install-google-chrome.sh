843216029ccc539d124c1a5d0969b8d08c882856
Konstantin Dyachenko <drakonard@gmail.com>
2022-11-07 20:14:04 +0300

    Better installation script.

--- install-google-chrome.sh
+++ install-google-chrome.sh
@@ -1,8 +1,6 @@
-wget https://dl-ssl.google.com/linux/linux_signing_key.pub -O /tmp/google.pub
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 
-gpg --no-default-keyring --keyring /etc/apt/keyrings/google-chrome.gpg --import /tmp/google.pub
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-echo 'deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install google-chrome-stable
